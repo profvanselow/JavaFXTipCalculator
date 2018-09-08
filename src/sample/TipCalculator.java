8b653974a6029f8982ff435dc03ab745c8ae7930
Prof. Vanselow <profvanselow@gmail.com>
2018-09-08 07:49:07 -0400

    Initial commit

---
+++ src/sample/TipCalculator.java
@@ -0,0 +1,40 @@
+package sample;
+
+// TipCalculator.java
+// Main application class that loads and displays the Tip Calculator's GUI.
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class TipCalculator extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
+
+        Scene scene = new Scene(root); // attach scene graph to scene
+        stage.setTitle("Tip Calculator"); // displayed in window's title bar
+        stage.setScene(scene); // attach scene to stage
+        stage.show(); // display the stage
+
+        //GridPane grid = new GridPane();
+        //grid.setAlignment(Pos.CENTER);
+
+        //Scene scene = new Scene(grid, 300,300);
+        //stage.setScene(scene);
+
+    }
+
+    public static void main(String[] args)
+    {
+        // create a TipCalculator object and call its start method
+        launch(args);
+    }
+}
+
+
